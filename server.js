8a589e41a59fedee667991761f233f89bc83bb63
Shebo <shebo6@gmail.com>
2016-12-26 19:40:57 +0200

    Init Commit

---
+++ server.js
@@ -0,0 +1,66 @@
+const fs = require('fs');
+const express = require('express');
+const hbs = require('hbs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
+
+// logger
+app.use(function(req, res, next){
+    var now = new Date().toString();
+    var log = `${now}: ${req.method} ${req.url}`;
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', function(err){
+        if(err){
+            console.log('Unable to save to log file');
+        }
+    });
+    next();
+});
+
+// under maintenance
+/*app.use(function(req, res, next){
+    res.render('maintenance', {
+        pagetitle: 'Under Maintenance...'
+    });
+});*/
+
+hbs.registerHelper('getCurrentYear', function(){
+    return new Date().getFullYear();
+});
+
+hbs.registerHelper('screamIt', function(text){
+    return text.toUpperCase();
+});
+
+
+app.get('/', function(req, res){
+    res.render('home', {
+        pagetitle: 'Home'
+    });
+});
+
+app.get('/maintenance', function(req, res){
+    res.render('maintenance', {
+        pagetitle: 'Under Maintenance...'
+    });
+});
+
+app.get('/about', function(req, res){
+    res.render('about', {
+        pagetitle: 'About'
+    });
+});
+
+app.get('/bad', function(req, res){
+    res.send({
+        err: 'No Page'
+    });
+});
+
+app.listen(3000, function(){
+    console.log('Server is up on port 3000');
+});
